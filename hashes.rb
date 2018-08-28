e95a68d43f1a20491726efec54fc841ad927240a
Eric Berthomier <berthomier.eric@gmail.com>
2018-08-28 23:06:00 +0200

    Exercice HASHES

--- hashes.rb
+++ hashes.rb
@@ -1,2 +1,20 @@
+#cle avec chaine
 hashes = {"nom"=>"nom1", "prenom"=>"prenom1", "age"=>24}
 puts hashes["nom"]
+
+#cle avec symbole
+hashes1 = {nom: "nom1", prenom:"prenom1", age:25}
+puts hashes1[:age]
+
+#ajout dans la liste
+hashes[:hobby] = "developper"
+hashes["metier"] = "informatique"
+#puts hashes
+puts hashes[:hobby]
+
+# supprimer de la liste
+hashes.delete("metier")
+hashes.delete(:hobby)
+puts hashes
+
+hashes.each {|key,value| puts "cle #{key} et la valeur #{value}"}
